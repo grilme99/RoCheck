6bafdbc0f25d69a599c0e1039c2707ff2a62220f
grilme99 <podcast46231@gmail.com>
2019-10-28 15:32:25 +0000

    First commit

--- index.js
+++ index.js
@@ -23,53 +23,63 @@ const request = require('request-promise') // Used to make HTTP requests
  * @param {Number} placeId
  * @param {String} jobId
  * @param {String} cookie
- * @return {String}
+ * @return {String} ipAddress
  * @api public
  */
-module.exports = async (placeId, jobId, cookie) => {
-	/**
-	 * Initialize the join request. Receives "joinScriptUrl" which will
-	 * reutrn lots of information about the game if successful.
-	 */
-	const initial_request = await request({
-		uri: `https://assetgame.roblox.com/Game/PlaceLauncher.ashx?request=RequestGameJob&placeId=${placeId}&gameId=${jobId}`,
-		headers: {
-			'User-Agent':
-				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-			Referer: `https://www.roblox.com/games/${placeId}/`,
-			Origin: 'https://www.roblox.com',
-			Cookie: `.ROBLOSECURITY=${cookie}; path=/; domain=.roblox.com;`
-		},
-		json: true // Automatically parses the JSON string in the response
-	})
-		.then(response => {
-			return response
-		})
-		.catch(error => {
-			throw Error(error)
-		})
-
-	/**
-	 * Check if "joinScriptUrl" was returned
-	 */
-	if (initial_request && initial_request.joinScriptUrl) {
+module.exports = (placeId, jobId, cookie) => {
+	return new Promise(async (resolve, reject) => {
 		/**
-		 * Use the "joinScriptUrl" to get information about the game,
-		 * including the game server IP (MachineAddress)
+		 * Initialize the join request. Receives "joinScriptUrl" which will
+		 * reutrn lots of information about the game if successful.
 		 */
-		const game_data = await request({
-			uri: initial_request.joinScriptUrl,
-			json: true
+		const initial_request = await request({
+			uri: `https://assetgame.roblox.com/Game/PlaceLauncher.ashx?request=RequestGameJob&placeId=${placeId}&gameId=${jobId}`,
+			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+				Referer: `https://www.roblox.com/games/${placeId}/`,
+				Origin: 'https://www.roblox.com',
+				Cookie: `.ROBLOSECURITY=${cookie}; path=/; domain=.roblox.com;`
+			},
+			json: true // Automatically parses the JSON string in the response
 		})
 			.then(response => {
 				return response
 			})
 			.catch(error => {
-				throw Error(error)
+				reject(error)
 			})
 
-		console.log(game_data)
-	} else {
-		throw Error('Error on initial request to Roblox')
-	}
+		/**
+		 * Check if "joinScriptUrl" was returned
+		 */
+		if (initial_request && initial_request.joinScriptUrl) {
+			/**
+			 * Use the "joinScriptUrl" to get information about the game,
+			 * including the game server IP (MachineAddress)
+			 */
+			let game_data = await request({
+				uri: initial_request.joinScriptUrl,
+				json: true
+			})
+				.then(response => {
+					return response
+				})
+				.catch(error => {
+					reject(error)
+				})
+
+			/**
+			 * Parse the JSON
+			 */
+			game_data = JSON.parse(game_data.toString().replace(/--.*\r\n/, ''))
+
+			/**
+			 * Return MachineAddress
+			 */
+			resolve(game_data.MachineAddress)
+		} else {
+			reject('Error on initial request to Roblox')
+		}
+	})
 }
